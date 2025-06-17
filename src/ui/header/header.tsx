cbc8399a9fec3e8ecfdcdeb5f6d5ed41ebef5a92
Himanshu Sharma <himanshu.st6@gmail.com>
2025-06-18 04:02:08 +0530

    App header UI

--- src/ui/header/header.tsx
+++ src/ui/header/header.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
+import Button from "../button/button";
+import "./header.scss";
+import Nav from "../nav/nav";
+
 export default function Header() {
+  const handleClick = () => {
+    console.log("Redirect to login page!");
+  }
+
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="header">
+      <section className="logo">
+        <Image width={58} height={30} src="/images/logo.png" alt="Logo" />
+      </section>
+      <section className="nav">
+        <Nav />
+      </section>
+      <section className="login">
+        <Button type="secondary" onClick={handleClick} isSmall isTransition>
+          <div>
+            <i className="fa-solid fa-right-to-bracket"></i>
+          </div>
+          <div>Sign In</div>
+        </Button>
+      </section>
     </div>
   );
 }
